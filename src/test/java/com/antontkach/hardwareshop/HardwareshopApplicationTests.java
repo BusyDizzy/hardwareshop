b23660efa68d76e0819c4f9dc2dcad52816e52e8
BusyDizzy <antontk@gmail.com>
2023-06-02 11:36:30 +0700

    Initial commit

---
+++ src/test/java/com/antontkach/hardwareshop/HardwareshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antontkach.hardwareshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
